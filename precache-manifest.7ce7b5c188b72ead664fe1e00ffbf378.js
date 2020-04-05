46254ad0097cfcf8aee44b04dec985a370e2d322
kulinsky <ankulinsky@gmail.com>
2020-04-05 17:27:09 +0300

    production build in root directory :)

---
+++ precache-manifest.7ce7b5c188b72ead664fe1e00ffbf378.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34e87e96b394dcbe88a02110da767d26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c83a29aeb36c40af76b",
+    "url": "/static/css/main.91b483b4.chunk.css"
+  },
+  {
+    "revision": "a8a10b32ff9442a50637",
+    "url": "/static/js/2.7865f59f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7865f59f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c83a29aeb36c40af76b",
+    "url": "/static/js/main.5b83c4dc.chunk.js"
+  },
+  {
+    "revision": "986b1c6d4bef0a432dea",
+    "url": "/static/js/runtime-main.8be54e6e.js"
+  }
+]);
